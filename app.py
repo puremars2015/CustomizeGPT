1e0e596a0c4a2f16b273ebe4153582393791562c
allytsai0320 <allytsai.0320@gmail.com>
2025-12-17 13:59:27 +0800

    測試POST&GET差異

--- app.py
+++ app.py
@@ -8,8 +8,38 @@ def home():
     return render_template('index.html')
 
 @app.route('/login')
-def home():
+def login():
     return render_template('login.html')
+
+@app.route('/login1')
+def login1():
+    return render_template('login1.html')
+
+@app.route('/login-action', methods=['GET', 'POST'])
+def login_action():
+
+    # 依照請求方式取得帳密
+    if request.method == 'GET':
+        account = request.args.get('account')
+        password = request.args.get('password')
+    else:
+        data = request.get_json()
+        account = data.get('account')
+        password = data.get('password')
+
+    # 驗證帳密
+    if account != 'sean.ma@thetainformation.com' or password != '1234567890':
+        return jsonify({
+            "status": "error",
+            "message": "Invalid credentials"
+        }), 401
+
+    # 登入成功
+    return jsonify({
+        "status": "success",
+        "token": "JDFHBVWHSJDHKS;12JNWTELVT"
+    })
+
 
 @app.route('/callAI', methods=['GET'])
 def call_ai():
